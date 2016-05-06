481e93b88228a7a8fcbd91bac6649c30184d1dda
jumaanji <cadarm_z@etna-alternance.net>
2016-05-06 18:51:32 +0200

    editview

--- MyWeatherBuddy/app/src/main/java/com/example/jumana/myweatherbuddy/MainActivity.java
+++ MyWeatherBuddy/app/src/main/java/com/example/jumana/myweatherbuddy/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.jumana.myweatherbuddy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.jumana.myweatherbuddy.data.model.City;
@@ -24,25 +27,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WeatherAPI.Factory.getInstance().getCity("paris,fr", APPID).enqueue(new Callback<City>() {
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<City> call, Response<City> response) {
-                String name = "City : " + response.body().getName() + "\n";
-                String weather = "Weather : " + response.body().getWeather().get(0).getMain() + " (" + response.body().getWeather().get(0).getDescription() + ")\n";
-                String temp = "Temperature : " + oneDigit.format(response.body().getMain().getTemp() - 273.15) + "°\n";
-                String temp_min = "Temparature minimum : " + oneDigit.format(response.body().getMain().getTempMin() - 273.15) + "°\n";
-                String temp_max = "Temparature maximum : " + oneDigit.format(response.body().getMain().getTempMax() - 273.15) + "°\n";
-                String humidity = "Humidity : " + response.body().getMain().getHumidity().toString() + "\n";
-                String clouds = "Clouds : " + response.body().getClouds().getAll().toString() + "\n";
-                String wind_speed = "Wind speed : " + response.body().getWind().getSpeed().toString() + "\n";
-                String wind_deg = "Wind Degree : " + response.body().getWind().getDeg().toString() + "\n";
-                cityname = (TextView) findViewById(R.id.CityName);
-                cityname.setText(name + weather + temp + temp_min + temp_max + humidity + clouds + wind_speed + wind_deg);
-            }
+            public void onClick(View v) {
+                String search = editText.getText().toString();
 
-            @Override
-            public void onFailure(Call<City> call, Throwable t) {
-                Log.e("Error", t.getMessage());
+                if (search != "" && search != null && !search.isEmpty()) {
+                    WeatherAPI.Factory.getInstance().getCity(search, APPID).enqueue(new Callback<City>() {
+                        @Override
+                        public void onResponse(Call<City> call, Response<City> response) {
+                            String name = "City : " + response.body().getName() + "\n";
+                            String weather = "Weather : " + response.body().getWeather().get(0).getMain() + " (" + response.body().getWeather().get(0).getDescription() + ")\n";
+                            String temp = "Temperature : " + oneDigit.format(response.body().getMain().getTemp() - 273.15) + "°\n";
+                            String temp_min = "Temparature minimum : " + oneDigit.format(response.body().getMain().getTempMin() - 273.15) + "°\n";
+                            String temp_max = "Temparature maximum : " + oneDigit.format(response.body().getMain().getTempMax() - 273.15) + "°\n";
+                            String humidity = "Humidity : " + response.body().getMain().getHumidity().toString() + "\n";
+                            String clouds = "Clouds : " + response.body().getClouds().getAll().toString() + "\n";
+                            String wind_speed = "Wind speed : " + response.body().getWind().getSpeed().toString() + "\n";
+                            String wind_deg = "Wind Degree : " + response.body().getWind().getDeg().toString() + "\n";
+                            cityname = (TextView) findViewById(R.id.CityName);
+                            cityname.setText(name + weather + temp + temp_min + temp_max + humidity + clouds + wind_speed + wind_deg);
+                        }
+
+                        @Override
+                        public void onFailure(Call<City> call, Throwable t) {
+                            Log.e("Error", t.getMessage());
+                        }
+                    });
+                }
+
             }
         });
 
